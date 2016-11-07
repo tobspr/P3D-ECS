8e98fcc11456b706af7bd4af983a6153a2425ecd
tobspr <tobias.springer1@googlemail.com>
2016-11-07 10:42:11 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,6 @@ import os, sys
 print("Starting to build ...")
 os.chdir("./p3d_ecs/native/")
 os.system('"' + sys.executable + '" update_module_builder.py')
-os.system('"' + sys.executable + '" build.py')
+os.system('"' + sys.executable + '" build.py ' + ' '.join(sys.argv))
 os.chdir(os.path.abspath(os.path.dirname(__file__)) or ".")
 sys.exit(0)
