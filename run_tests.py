826c8d8831607c5c5a3d91cc34d4b014a53b29fc
tobspr <tobias.springer1@googlemail.com>
2016-11-07 10:43:08 +0100

    Update run_tests.py

--- run_tests.py
+++ run_tests.py
@@ -2,6 +2,7 @@
 import inspect
 import panda3d
 import os
+import sys
 import panda3d.core
 
 import p3d_ecs.native.p3d_ecs as p3d_ecs
@@ -15,8 +16,14 @@ except:
 ALL_TESTSUITES = [i for i, _ in inspect.getmembers(p3d_ecs) if i.startswith("testsuite_")]
 print("Suites:", "\n" + '\n'.join(ALL_TESTSUITES))
 
+
 # TODO: Make this configurable with command line args
 suites_to_run = ALL_TESTSUITES[:]
+
+
+if "--noperf" in sys.argv:
+    print("Skipping performance tests")
+    suites_to_run.remove("testsuite_perftest")    
 
 for suite in suites_to_run:
     print("\n\n" + "=" * 10, "Running test suite", suite, "=" * 10, "\n")
