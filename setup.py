848c3d369e996b9479795e67d0b82a56695be4c7
tobspr <tobias.springer1@googlemail.com>
2016-11-07 09:03:25 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 # TODO: Write real setup script
 
+print("Importing panda3d")
+import panda3d
+
 import os, sys
 print("Starting to build ...")
 os.chdir("./p3d_ecs/native/")
 os.system('"' + sys.executable + '" update_module_builder.py')
 os.system('"' + sys.executable + '" build.py')
-os.chdir(os.path.dirname(__file__))
+os.chdir(os.path.abspath(os.path.dirname(__file__)) or ".")
 sys.exit(0)
