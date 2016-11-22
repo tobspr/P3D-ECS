08dd63ed8d1d7ac464133b44dd570b0232e3bac6
tobspr <tobias.springer1@googlemail.com>
2016-11-22 08:33:24 +0100

    Update compile_proto_files.py

--- p3d_ecs/scripts/compile_proto_files.py
+++ p3d_ecs/scripts/compile_proto_files.py
@@ -7,7 +7,7 @@ from os.path import realpath, abspath, join, dirname
 
 this_dir = abspath(realpath(dirname(__file__)))
 base_dir = abspath(join(this_dir, "..", ".."))
-proto_src_dir = abspath(join(base_dir, "p3d_ecs", "Proto"))
+proto_src_dir = abspath(join(base_dir, "p3d_ecs", "proto"))
 
 
 if sys.platform == "win32":
